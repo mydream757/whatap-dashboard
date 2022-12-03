5da7abe3dc3b5c7ec9bddb806e7e9ea6eaaa8aed
Joonyoung Choi <mydream757@gmail.com>
2022-12-03 18:24:00 +0900

    Test : useSpotConnections hook 을 이용한 api 다중 요청 테스트
    * api 요청이 동시에 이루어졌을 때 동작 방식을 파악하기 위함
    
    TODO : 네트워크 요청을 관장하는 별도의 로직 필요

--- src/components/routes/Dashboard.tsx
+++ src/components/routes/Dashboard.tsx
@@ -1,30 +1,57 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useMemo } from 'react';
 import { Col, Layout, Row } from 'antd';
 import Widget, { WidgetProps } from '../widgets/Widget';
-
-const testWidgetDatum: WidgetProps[] = [
-  {
-    header: {
-      title: 'Widget 1',
-      features: [
-        {
-          type: 'iconButton',
-          iconKey: 'arrow-right',
-        },
-      ],
-    },
-    body: {
-      data: '',
-    },
-  },
-];
+import { format } from 'date-fns';
+import { useSpotConnections } from '../../hooks/useSpotConnections';
 
 export default function Dashboard(): ReactElement {
+  const apiResult = useSpotConnections({
+    apis: [
+      'cpu',
+      'act_agent',
+      'act_sql',
+      'threadpool_active',
+      'cpucore',
+      'dbc_active',
+      'dbc_idle',
+      'tps',
+      'txcount',
+    ],
+  });
+
+  const parsedCommon = useMemo((): WidgetProps[] => {
+    return Object.keys(apiResult).map((key) => {
+      const data = apiResult[key as keyof typeof apiResult]?.data;
+
+      return {
+        header: {
+          title: key,
+        },
+        body: {
+          chartProps: {
+            type: 'bar' as const,
+            data: {
+              labels: data?.map((data) => format(new Date(data.time), 'mm:ss')),
+              datasets: [
+                {
+                  type: 'bar',
+                  label: key,
+                  data: (data || []).map((data) => data.value),
+                  borderColor: 'rgb(75, 192, 192)',
+                },
+              ],
+            },
+          },
+        },
+      };
+    });
+  }, [apiResult]);
+
   return (
     <Layout style={{ padding: '8px' }}>
-      <Row gutter={8} wrap>
-        {testWidgetDatum.map((props, index) => (
-          <Col flex={1} key={`${index}`}>
+      <Row gutter={[8, 8]} wrap>
+        {parsedCommon.map((props, index) => (
+          <Col key={`${index}`}>
             <Widget {...props} />
           </Col>
         ))}
